ee6f0589da0f8eefa42d3ba46c559cae8fd15f85
jkworks <info@jilliankayworks.com>
2021-07-12 20:52:44 -0600

    changing concat to join

--- script.js
+++ script.js
@@ -1,62 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// storing answers to prompts
-var passwordLength
-var uppercase
-var lowercase
-var specialCharacter 
-var numbers
 
 //password prompt arrays
-var allSpecial = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
+var allSpecial = "!@#%^&*?()<>/-+".split("");
 var allUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var allLowercase = "abcdefghijklmnopqrstuvwxyz".split("");
 var allNumbers = "0123456789".split("");
-var password = []
 
 //what length between 8 and 128
-function userPrompts() {
-  passwordLength = prompt('How many characters? Must be between 8 and 128')
+function generatePassword() {
+  passwordLength = prompt('How many characters? Must be between 8 and 128');
   // defined function need to be stored it's in the var
   if (passwordLength < 8) {
-    alert('too short!')
-    userPrompts()
+    alert('hey now this password is too short.. make it more than 8!');
+    generatePassword();
   } else if (passwordLength > 128){
-    alert('too long!')
-    userPrompts()
+    alert('woah... this password is too long make it less than 128!');
+    generatePassword();
   } else {
     passwordLength
   }
 
 //prompts sc , lc, up, !, numbers
-  specialCharacter = confirm('Do you want special characters?') 
-  uppercase = confirm('Do you want uppercase?')
-  lowercase = confirm('Do you want lowercase?')
-  numbers = confirm('Do you want numbers?')
-  passwordChoices = []
+  var specialCharacter = confirm('Do you want special characters?') 
+  var uppercase = confirm('Do you want uppercase?')
+  var lowercase = confirm('Do you want lowercase?')
+  var numbers = confirm('Do you want numbers?')
+  var passwordChoices = []
+
+  if (specialCharacter) {
+    passwordChoices = (allSpecial.join(passwordChoices))
+  }  
+  if (uppercase) {
+    passwordChoices = passwordChoices.join(allUppercase);
+    console.log(passwordChoices);
+  }  
+  if (lowercase) {
+    passwordChoices = passwordChoices.join(allLowercase);
+    console.log(passwordChoices);
+  }  
+  if (numbers) {
+    passwordChoices = passwordChoices.join(allNumbers);
+    console.log(passwordChoices);
+  }  
+
+ //cannot figure out how to write a for loop to pull random numbers from selections 
+  return passwordChoices;
 
 }
 
-function generatePassword() {
-
-  if (specialCharacter === true) {
-    passwordChoices = passwordChoices.concat(specialCharacter);
-  }  
-  if (uppercase === true) {
-    passwordChoices = passwordChoices.concat(uppercase);
-  }  
-  if (lowercase === true) {
-    passwordChoices = passwordChoices.concat(lowercase);
-  }  
-  if (numbers === true) {
-    passwordChoices = passwordChoices.concat(numbers);
-  }  
-
-  return passwordChoices
-}
 
 function writePassword() {
-  userPrompts()
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
